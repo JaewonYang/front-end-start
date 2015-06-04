7707cee9cf5428f3e016095db8d2e4b606711f6d
Jaewon <skyboyddd@gmail.com>
2015-06-04 20:21:54 +0900

    08 start

---
+++ 08/app.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Jaewon on 2015-06-04.
+ */
+var cards = [1,2,3,4,5,1,2,3,4,5];
+
+var $board = $('#gameBoard');
+
+//섞기
+cards = shuffle(cards);
+console.log(cards)
+
+//깔기
+var doms = [];
+for(var i=0; i < cards.length; i++){
+
+    doms.push('<div class="card">' + cards[i]+ '</div>');
+}
+
+$board.html(doms.join(''));
+
+.addEventListener('click', function (event) {
+
+    $('gameBoard.card').css("background-color","white");
+})
---
+++ 08/util.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Jaewon on 2015-06-04.
+ */
+function shuffle(o){
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+}
