3cb9d8fa1750990938d649cb2e46209e8638dc22
Jaewon <skyboyddd@gmail.com>
2015-06-13 23:30:48 +0900

    기말고사 Note Setting Complete

---
+++ mice_it_Final_Exam/js/src/config.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Jaewon on 2015-06-13.
+ */
+var NoteApp = {
+    storageKey : 'note'
+};
---
+++ mice_it_Final_Exam/js/src/util.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Jaewon on 2015-06-13.
+ */
+
+(function(app){
+
+    app.util = {
+        storage : {
+            load: function () {
+                console.log('storage.load()');
+                return localStorage.getItem(app.storageKey);
+                // return JSON.parse(localStorage.getItem(storageKey) || "[]");
+            },
+            save: function (event, data) {
+                console.log('storage.save()');
+                // localStorage.setItem(storageKey, JSON.stringify(data));
+                localStorage.setItem(app.storageKey, data);
+            }
+        }
+    };
+
+})(NoteApp);
